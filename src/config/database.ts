93f6018389ed2a01f06807bbae467d06996b2e4b
newfox79 <znaykaznaykovich@gmail.com>
2020-09-07 06:44:28 +0300

    update(config): handle credentials + correct document name

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,18 @@
 export const domain: string = process.env.DATABASE || 'localhost';
 export const port: string = process.env.DATABASE_PORT || '27017';
-export const doc: string = process.env.DATABASE_DOCUMENT || 'mycargarage';
+export const doc: string = process.env.DATABASE_DOCUMENT || 'sde-service';
+export const user: string = process.env.MONGO_INITDB_ROOT_USERNAME || 'root';
+export const password: string = process.env.MONGO_INITDB_ROOT_PASSWORD || 'root';
 
 export const url: string = `mongodb://${domain}:${port}/${doc}`;
 
-export default { url };
+import { ConnectionOptions } from 'mongoose';
+
+export const options: ConnectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  pass: password,
+  user,
+};
+
+export default { url, options };
