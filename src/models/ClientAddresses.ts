8fb780b8cd930a0fe6ab38c304cbf8a0e7c378e2
newfox79 <znaykaznaykovich@gmail.com>
2020-09-07 06:46:58 +0300

    remove(models): remove old style models

--- src/models/ClientAddresses.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import { clientAddressShema } from './sharedSubShemas';
-
-export const ClientAddressesShema = new mongoose.Schema({
-  addresses: [clientAddressShema],
-});
-
-export default mongoose.model('ClientAddresses', ClientAddressesShema);
